21dcb33ce5fbb692c88a475dcb8070feb653f7fc
Strict_duck <azamatabayevnis@gmail.com>
2017-10-07 18:00:25 +0600

    sad

---
+++ app.js
@@ -0,0 +1,16 @@
+(function(){
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAr_YxvV04UPsL3Th8oU8sXeIoJNlHs7Do",
+    authDomain: "kono-f4f2d.firebaseapp.com",
+    databaseURL: "https://kono-f4f2d.firebaseio.com",
+    projectId: "kono-f4f2d",
+    storageBucket: "kono-f4f2d.appspot.com",
+    messagingSenderId: "177948466004"
+  };
+  firebase.initializeApp(config);
+      
+      
+      
+      
+  }());
